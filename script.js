316ca8cf09893411dcfa28209ad8cd6853fc093f
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2025-01-10 17:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const imageUrls = [
-    { url: 'https://i.redd.it/8pkohui3he821.jpg' },
-    { url: 'https://wonderfulengineering.com/wp-content/uploads/2016/02/wallpaper-background-2.jpg' },
-    { url: 'https://res.allmacwallpaper.com/get/iMac-27-inch-wallpapers/Godafoss,-Iceland-2560x1440/19538-10.jpg' }
+  { url: "https://picsum.photos/id/237/200/300" },
+  { url: "https://picsum.photos/id/238/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
 
 document.getElementById('download-images-button').addEventListener('click', () => {
     downloadImages(imageUrls)
@@ -23,7 +25,6 @@ function downloadImages(urls) {
             img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
         })
     );
-
     return Promise.all(promises);
 }
 
@@ -31,8 +32,39 @@ function displayImages(images) {
     const outputDiv = document.getElementById('output');
     console.log(outputDiv);
     
-    outputDiv.innerHTML = ''; // Clear any previous images
+    outputDiv.innerHTML = ''; 
+    images.forEach(img => {
+        outputDiv.appendChild(img);
+    });
+}
 
+document.getElementById('download-images-button').addEventListener('click', () => {
+    downloadImages(imageUrls)
+        .then(images => {
+            displayImages(images);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
+function downloadImages(urls) {
+    const promises = urls.map(image =>
+        new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = image.url;
+            img.onload = () => resolve(img);
+            img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+        })
+    );
+    return Promise.all(promises);
+}
+
+function displayImages(images) {
+    const outputDiv = document.getElementById('output');
+    console.log(outputDiv);
+    
+    outputDiv.innerHTML = ''; 
     images.forEach(img => {
         outputDiv.appendChild(img);
     });
